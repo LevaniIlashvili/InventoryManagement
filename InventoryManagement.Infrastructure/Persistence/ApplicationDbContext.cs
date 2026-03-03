62d7a277c26619f8074b37d16801d8d969772f99
Levan Ilashvili <levanilashvili2005@gmail.com>
2026-03-03 19:53:57 +0400

    change folder location

---
+++ InventoryManagement.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using InventoryManagement.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Infrastructure.Persistence;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public DbSet<Inventory> Inventories { get; set; }
+    public DbSet<InventoryCategory> InventoryCategories { get; set; }
+    public DbSet<InventoryTag> InventoryTags { get; set; }
+    public DbSet<InventoryItem> InventoryItems { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
