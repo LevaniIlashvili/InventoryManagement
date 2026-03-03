46d585c8b40d6214c849f6e50573762ff680ee46
Levan Ilashvili <levanilashvili2005@gmail.com>
2026-03-03 19:52:47 +0400

    add exceptions

---
+++ InventoryManagement.Application/Exceptions/ForbiddenException.cs
@@ -0,0 +1,6 @@
+﻿namespace InventoryManagement.Application.Exceptions;
+
+public class ForbiddenException : Exception
+{
+    public ForbiddenException(string message) : base(message) { }
+}
---
+++ InventoryManagement.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,6 @@
+﻿namespace InventoryManagement.Application.Exceptionsl;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string message) : base(message) { }
+}
